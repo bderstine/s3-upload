02a345facba228bd12ed2b423a62b42e9422350e
Brad <brad@bizzartech.com>
2016-10-24 18:18:03 +0000

    updates

--- app.js
+++ app.js
@@ -3,15 +3,12 @@ var formidable = require('formidable');
 var http = require('http');
 var util = require('util');
 var AWS  = require('aws-sdk');
-var config = require('./config');
+var config = require('config');
 var s3 = new AWS.S3({
     accessKeyId: config.get('S3_ACCESS_KEY'),
     secretAccessKey: config.get('S3_SECRET_KEY'),
     apiVersion: '2006-03-01'
 });
-var s3Stream = require('s3-upload-stream')(s3);
-var bucket = 'bucket-name';
-var key = 'abcdefgh';
 
 
 http.createServer(function(req, res) {
@@ -19,17 +16,15 @@ http.createServer(function(req, res) {
     if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
 
         var form = new formidable.IncomingForm();
+
         form.on('progress', function(bytesReceived, bytesExpected) {
-            //console.log('onprogress', parseInt( 100 * bytesReceived / bytesExpected ), '%');
+            console.log('onprogress', parseInt( 100 * bytesReceived / bytesExpected ), '%');
         });
 
         form.on('error', function(err) {
             console.log('err',err);
         });
 
-        // This 'end' is for the client to finish uploading
-        // upload.on('uploaded') is when the uploading is
-        // done on AWS S3
         form.on('end', function() {
             console.log('ended!!!!', arguments);
         });
@@ -38,50 +33,17 @@ http.createServer(function(req, res) {
             console.log('aborted', arguments);
         });
 
-        form.onPart = function(part) {
-            console.log('part',part);
-            // part looks like this
-            //    {
-            //        readable: true,
-            //        headers:
-            //        {
-            //            'content-disposition': 'form-data; name="upload"; filename="00video38.mp4"',
-            //            'content-type': 'video/mp4'
-            //        },
-            //        name: 'upload',
-            //            filename: '00video38.mp4',
-            //        mime: 'video/mp4',
-            //        transferEncoding: 'binary',
-            //        transferBuffer: ''
-            //    }
+	form.on('file', function(name, file, arguments){
+            console.log(arguments);
+            //var a = arguments;
+            //console.log(a[1]['name']);
+            //console.log(a[1]['path']);
+        });
 
-            var start = new Date().getTime();
-            var upload = s3Stream.upload({
-                "Bucket": bucket,
-                "Key": part.filename
-            });
-
-            // Optional configuration
-            //upload.maxPartSize(20971520); // 20 MB
-            upload.concurrentParts(5);
-
-            // Handle errors.
-            upload.on('error', function (error) {
-                console.log('errr',error);
-            });
-            upload.on('part', function (details) {
-                console.log('part',details);
-            });
-            upload.on('uploaded', function (details) {
-                var end = new Date().getTime();
-                console.log('it took',end-start);
-                console.log('uploaded',details);
-            });
-
-            // Maybe you could add compress like
-            // part.pipe(compress).pipe(upload)
-            part.pipe(upload);
-        };
+        //var s3 = new AWS.S3();
+        //var params = {Bucket: 'bizzar-s3-upload', Key: 'myImageFile.jpg'};
+        //var file = require('fs').createWriteStream('/path/to/file.jpg');
+        //s3.getObject(params).createReadStream().pipe(file);
 
         form.parse(req, function(err, fields, files) {
             res.writeHead(200, {'content-type': 'text/plain'});
